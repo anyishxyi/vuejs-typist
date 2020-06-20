ebfe269320c8706c5dca8d5f175bfc04c5d756aa
jeanpaul ngalula <jeanpaulngalula07@gmail.com>
2020-06-20 23:21:05 +0200

    build for prod

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import vuejsTypist from './components/vuejs-typist'
+
+Vue.component('vuejsTypist', vuejsTypist);
+
 Vue.config.productionTip = false
 
 new Vue({
